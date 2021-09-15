50118b3a2ad82d5023bf56535ab7fe7d63105063
Katherine Kwok <kkwok1@swarthmore.edu>
2021-09-14 21:40:13 -0500

    Finished write up

--- HW 1/julia_code/02_growth_compute.jl
+++ HW 1/julia_code/02_growth_compute.jl
@@ -18,20 +18,20 @@ time = @elapsed Solve_model(prim, res) # solve the model!
 #----------------#
 
 #value function
-Plots.plot(k_grid, val_func[:, 1], label = "Good State", title="Value Function")
-Plots.plot!(k_grid, val_func[:, 2], label = "Bad State", title="Value Function")
+Plots.plot(k_grid, val_func[:, 1], label = "Good State")
+Plots.plot!(k_grid, val_func[:, 2], label = "Bad State", title="Value Function (Julia)")
 Plots.savefig("02_Value_Functions.png")
 
 #policy functions
-Plots.plot(k_grid, pol_func[:, 1], label = "Good State", title="Policy Function")
-Plots.plot!(k_grid, pol_func[:, 2], label = "Bad State", title="Policy Function")
+Plots.plot(k_grid, pol_func[:, 1], label = "Good State")
+Plots.plot!(k_grid, pol_func[:, 2], label = "Bad State", title="Policy Function (Julia)")
 Plots.savefig("02_Policy_Functions.png")
 
 #changes in policy function
 pol_func_δ_good = copy(pol_func[:, 1]).-k_grid
 pol_func_δ_bad = copy(pol_func[:, 2]).-k_grid
-Plots.plot(k_grid, pol_func_δ_good, label = "Good State",title="Policy Functions Changes")
-Plots.plot!(k_grid, pol_func_δ_bad, label = "Bad State",title="Policy Functions Changes")
+Plots.plot(k_grid, pol_func_δ_good, label = "Good State")
+Plots.plot!(k_grid, pol_func_δ_bad, label = "Bad State",title="Policy Functions Changes (Julia)")
 Plots.savefig("02_Policy_Functions_Changes.png")
 
 println("Completed program in $time seconds")
