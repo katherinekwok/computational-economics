210ce2f873a1fc0a449b328e84df3b6c3539f433
Katherine Kwok <kkwok1@swarthmore.edu>
2021-10-23 15:35:22 -0500

    Fixed the previous error - mixed up functions for calculating r and w; made main algorithm function and cleaned up code to run entire experiment now.

--- HW 4/code/TP_and_functions.jl
+++ HW 4/code/TP_and_functions.jl
@@ -11,8 +11,9 @@
 #   (1) functions for shooting backward
 #   (2) functions for shooting forward
 #   (3) functions for checking for convergence
+#   (4) functions for running the main algorithm and producing results + plots
 
-include("models_and_functions_SS.jl") # use functions that find steady state solution
+include("model_and_functions.jl") # use functions that find steady state solution
                                       # if function referenced in this file is not defined here
                                       # then it is in the SS file
 
@@ -40,32 +41,34 @@ end
 
 # initialize_TP: This function solves for the steady states, then initializes the
 #                transition path variables (stored in the Results_TP struc).
-function initialize_TP()
-    p0, r0, w0, cv0 = solve_model()           # solve θ = 0.11 model - with soc security
-    pT, rT, wT, cvT = solve_model(θ_0 = 0.0)  # solve θ = 0 model - no soc security
+#
+# NOTE: by default, this function initializes a transtion path for unanticipated
+#       social security shock
+function initialize_TP(p0::Primitives, pT::Primitives, TPs::Int64; date_implemented::Int64 = 1)
 
-    TPs = 30 # number of transition periods
+    println("-----------------------------------------------------------------------")
+    @printf "          Starting Outer While Loop with %d transition periods \n" TPs
+    println("-----------------------------------------------------------------------")
 
     @unpack α, δ, μ_r, na, nz, N, age_retire = p0 # unpack prims from θ = 0.11 model (same for θ = 0)
-    θ_TP = repeat([pT.θ], TPs) # θ along the transition path
+
+    θ_TP = vcat(repeat([p0.θ], date_implemented - 1), repeat([pT.θ], TPs - date_implemented + 1))
 
     K_TP = collect(range(p0.K_0, step = ((pT.K_0 - p0.K_0)/TPs), stop = pT.K_0))[2:TPs+1] # transition path of K
     L_TP = collect(range(p0.L_0, step = ((pT.L_0 - p0.L_0)/TPs), stop = pT.L_0))[2:TPs+1] # transition path of L
-    r_TP = F_1.(α, K_TP, L_TP)     # transition path of r using K_TP, L_TP
-    w_TP = F_2.(α,δ, K_TP, L_TP)   # transition path of w using K_TP, L_TP
-    b_TP = calculate_b.(θ_TP, w_TP, L_TP, μ_r)
+    r_TP = F_2.(α, δ, K_TP, L_TP)               # transition path of r using K_TP, L_TP
+    w_TP = F_1.(α, K_TP, L_TP)                  # transition path of w using K_TP, L_TP
+    b_TP = calculate_b.(θ_TP, w_TP, L_TP, μ_r)  # transition path of b
 
     col_num_all = (N - age_retire + 1) + (age_retire - 1)*nz # number of indices corresponding to age + state for everyone (carry-over from pset 3 set up)
     col_num_worker = (age_retire - 1)*nz                     # number of indices corresponding to age + state for workers  (carry-over from pset 3 set up)
-    val_func_TP = zeros(TPs, na, col_num_all)          # initial value function for all periods
-    pol_func_TP = zeros(TPs, na, col_num_all)          # initial policy function for all periods
-    lab_func_TP = zeros(TPs, na, col_num_worker)       # initial labor function for all periods
-    ψ_TP = zeros(TPs, na * nz, N)                      # initial cross-sec distribution for all periods
+    val_func_TP = zeros(TPs, na, col_num_all)                # initial value function for all periods
+    pol_func_TP = zeros(TPs, na, col_num_all)                # initial policy function for all periods
+    lab_func_TP = zeros(TPs, na, col_num_worker)             # initial labor function for all periods
+    ψ_TP = zeros(TPs, na * nz, N)                            # initial cross-sec distribution for all periods
 
-    tp = TransitionPaths(TPs, K_TP, L_TP, θ_TP, r_TP, w_TP, b_TP, val_func_TP,
-    pol_func_TP, lab_func_TP, ψ_TP) # transition path of b
-
-    tp, p0, r0, pT, rT # return initialized struc
+    tp = TransitionPaths(TPs, K_TP, L_TP, θ_TP, r_TP, w_TP, b_TP, val_func_TP, pol_func_TP, lab_func_TP, ψ_TP)
+    tp # return initialized struc
 end
 
 ##
@@ -83,8 +86,6 @@ function shoot_backward(pt::Primitives, tp::TransitionPaths, r0::Results, rT::Re
 
     for t in TPs-1:-1:1 # iterate from T back to 0
 
-        rt = initialize_results(pt) # initialize results
-
         # set up primitives for this backward induction of household dynamic programming problem
         # accessing using index + 1 because julia does not do 0-based indexing
         pt.θ = tp.θ_TP[t]     # θ
@@ -93,6 +94,8 @@ function shoot_backward(pt::Primitives, tp::TransitionPaths, r0::Results, rT::Re
         pt.b = tp.b_TP[t]     # social security benefit
         pt.K_0 = tp.K_TP[t]   # aggregate capital
         pt.L_0 = tp.L_TP[t]   # aggregate labor supply
+
+        rt = initialize_results(pt) # initialize results
 
         v_backward_iterate(pt, rt; steady_state = false, res_next_input = rt_next) # backward iterate
 
@@ -183,24 +186,37 @@ end
 # display_progress: This function plots the new and old transition paths for
 # troubleshooting
 function display_progress(tp::TransitionPaths, K_TP_1::Array{Float64}, L_TP_1::Array{Float64},
-    p0::Primitives, pT::Primitives)
+    p0::Primitives, pT::Primitives, experiment::String; save::Bool = false)
     @unpack TPs = tp
 
-    display(plot([tp.K_TP K_TP_1 repeat([p0.K_0], TPs) repeat([pT.K_0], TPs)],
+    # plot aggregate capital
+    K_plot = plot([tp.K_TP K_TP_1 repeat([p0.K_0], TPs) repeat([pT.K_0], TPs)],
             label = ["Old TP" "New TP" "SS w/ θ > 0" "SS w/ θ = 0"],
-            title = "Capital", legend = :topleft), )
+            title = "Aggregate Capital Transition Path", legend = :bottomright,
+            ylims = (3.25, 4.75))
+    display(K_plot)
 
-    display(plot([tp.L_TP L_TP_1 repeat([p0.L_0], TPs) repeat([pT.L_0], TPs)],
+    # plot aggregate labor
+    L_plot = plot([tp.L_TP L_TP_1 repeat([p0.L_0], TPs) repeat([pT.L_0], TPs)],
             label = ["Old TP" "New TP" "SS w/ θ > 0" "SS w/ θ = 0"],
-            title = "Labor", legend = :topleft))
+            title = "Aggregate Labor Transition Path", legend = :bottomright,
+            ylims = (0.34, 0.37))
+    display(L_plot)
+
+    if save == true
+        savefig(K_plot, "output/K_TP_"*experiment*"_"*string(TPs)*".png")
+        savefig(L_plot, "output/L_TP_"*experiment*"_"*string(TPs)*".png")
+    end
 end
 
 
 # check_convergence_TP: This function checks if the maximum, absolute difference
 # between K and L transition paths combined is less than the tolerance value.
 # If so, we have converged, and if not, we update the transition path and repeat.
-function check_convergence_TP(pt::Primitives, tp::TransitionPaths, K_TP_1::Array{Float64}, L_TP_1::Array{Float64};
+function check_convergence_TP(iter::Int64, pt::Primitives, tp::TransitionPaths,
+    K_TP_1::Array{Float64}, L_TP_1::Array{Float64}, experiment::String;
     tol::Float64 = 1.0e-3, λ::Float64 = 0.5)
+
     @unpack α, δ, μ_r = pt
     @unpack TPs, K_TP, L_TP, θ_TP = tp
 
@@ -210,6 +226,8 @@ function check_convergence_TP(pt::Primitives, tp::TransitionPaths, K_TP_1::Array
 
     if max_diff > tol # if max diff is above tolerance, we update
 
+        display_progress(tp, K_TP_1, L_TP_1, p0, pT, experiment) # plot current transition path
+
         tp.K_TP = λ .* K_TP_1 .+ (1-λ) .* K_TP # adjust using λ paramter
         tp.L_TP = λ .* L_TP_1 .+ (1-λ) .* L_TP
 
@@ -217,12 +235,96 @@ function check_convergence_TP(pt::Primitives, tp::TransitionPaths, K_TP_1::Array
         tp.w_TP = F_2.(α, δ, tp.K_TP, tp.L_TP)               # transition path of w
         tp.b_TP = calculate_b.(θ_TP, tp.w_TP, tp.L_TP, μ_r)  # transition path of θ
 
-        converged = 0 # update convergence flag
+        converged = 0    # convergence flag still 0
+
+        println("-----------------------------------------------------------------------")
+        @printf "       Completed %d iterations of inner while loop; continuing...\n" iter
+        println("-----------------------------------------------------------------------")
+
     else
-        converged = 1
+
+        display_progress(tp, K_TP_1, L_TP_1, p0, pT, experiment; save = true) # plot!
+
+        converged = 1    # update convergence flag!
+        tp.K_TP = K_TP_1 # store the converged transition paths
+        tp.L_TP = L_TP_1
+
+        println("-----------------------------------------------------------------------")
+        @printf "           Inner while loop converged after %d iteration\n" iter
+        println("-----------------------------------------------------------------------")
     end
     converged # return convergence flag
 end
 
-function check_convergence_outer()
+function check_convergence_SS(pT::Primitives, tp::TransitionPaths, TPs::Int64, iter::Int64; tol::Float64 = 1.0e-3)
+
+    # calculate diff between K and L in T (last period of transition) and steady state
+    diff = abs(tp.K_TP[TPs] - pT.K_0) + abs(tp.L_TP[TPs] - pT.L_0)
+
+    if diff > tol                 # if difference greater than tolerance
+        update_TPs = TPs + 10     # lengthen transition period by 10
+        converged = 0
+    else
+        converged = 1      # update convergence flag!
+        println("-----------------------------------------------------------------------")
+        @printf "           Outer while loop converged after %d iteration\n" iter
+        println("-----------------------------------------------------------------------")
+    end
+
+    converged, update_TPs # return convergence flag and updated transition period
+end
+
+##
+# ---------------------------------------------------------------------------- #
+#   (4) functions for running the main algorithm and producing results + plots
+# ---------------------------------------------------------------------------- #
+
+# solve_algorithm: This function runs the outer and inner while loops that solves
+#                  the Conesa-Krueger model along the transition paths.
+#
+#   (1) The inner loop takes a given transition period length (TPs), and shoots
+#       backward and forward to solve the HH problem and stationary distribution
+#       until the transition paths converged.
+#
+#   (2) The outer loop checks if the aggregate K, L last period of the transition
+#       is close enough to the ending steady state (no social security). If it's
+#       not close enough, the transition period is lengthened.
+
+function solve_algorithm(experiment::String, TPs::Int64)
+
+    converged_outer = 0          # convergence flag for outer while loop
+    iter_outer = 1               # iteration counter for outer while loop
+
+    while converged_outer == 0   # outer loop
+
+        # initialize transition path variables
+        tp = initialize_TP(p0, pT, TPs)
+        # initialize mutatable struc primitives for current period (t)
+        pt = initialize_prims()
+
+        K_TP_1 = zeros(tp.TPs)   # initialize arrays for new transition path
+        L_TP_1 = zeros(tp.TPs)
+        converged_inner = 0      # convergence flag for inner while loop
+        iter_inner = 1           # iteration counter for inner while loop
+
+        while converged_inner == 0 # inner loop
+
+            # shoot backwards from T-1 to 1 to solve dynamic household programming
+            @time shoot_backward(pt, tp, r0, rT)
+
+            # shoot forwards from 1 to T to solve cross-sec distribution for age and time
+            @time shoot_forward(pt, tp, r0, K_TP_1, L_TP_1)
+
+            # check progress and convergence
+            converged_inner = check_convergence_TP(iter_inner, pt, tp, K_TP_1, L_TP_1, experiment)
+            iter_inner += 1   # update iteration counter
+        end
+
+        # check outer convergence, update
+        converged_outer, update_TPs = check_convergence_SS(pT, tp, TPs, iter_outer)
+        TPs = update_TPs # update number of transition periods
+        iter_outer += 1
+    end
+
+    tp, pt # return converged transition paths and other stuff
 end
--- HW 4/code/main_program.jl
+++ HW 4/code/main_program.jl
@@ -5,44 +5,63 @@
 # paths from eliminating social security, using the Conesa-Krueger Model. We
 # solve for the transition paths for two cases:
 #
+#       (0) load functions, packages, compute steady states
 #       (1) unexpected elimination of social security
 #       (2) expected elimination of social security
 
 # ------------------------------------------------------------------------ #
-#  (0) load packages, functions, initialize
+#  (0) load packages, functions; compute steady states
 # ------------------------------------------------------------------------ #
 
 using Distributed, SharedArrays                # load package for running julia in parallel
 using CSV, DataFrames                          # load packages for exporting data to csv
 using Parameters, Plots, Printf, LinearAlgebra # load standard packages
-include("models_and_functions_SS.jl")           # import all functions and strucs
-include("models_and_functions_TP.jl")
+
+include("model_and_functions.jl")              # import all functions and strucs
+include("TP_and_functions.jl")
+
+p0, r0, w0, cv0 = solve_model()                # solve θ = 0.11 model - with soc security
+pT, rT, wT, cvT = solve_model(θ_0 = 0.0)       # solve θ = 0 model    - with no soc security
 
 # ------------------------------------------------------------------------ #
 #  (1) compute transition path for unexpected elimination of social security
 # ------------------------------------------------------------------------ #
 
-# initialize transition path variables, both steady states prims (p1), results (r1)
-# for θ = 0.11 (t=1) and prims (pT), results (rT) for θ = 0 (t=T)
-tp, p0, r0, pT, rT = initialize_TP()
+experiment = "unanticipated" # type of experiment
+TPs = 30                     # initial number of transition periods
 
-pt = initialize_prims() # initialize mutatable struc primitives for current period (t)
-converged = 0
+converged_outer = 0          # convergence flag for outer while loop
+iter_outer = 1               # iteration counter for outer while loop
 
-while converged == 0
-    # shoot backwards from T-1 to 1 to solve dynamic household programming
-    @time shoot_backward(pt, tp, r0, rT)
+while converged_outer == 0
 
-    # shoot forwards from 1 to T to solve cross-sec distribution for age and time
-    @time shoot_forward(pt, tp, r0, K_TP_1, L_TP_1)
+    # initialize transition path variables
+    tp = initialize_TP(p0, pT, TPs)
+    # initialize mutatable struc primitives for current period (t)
+    pt = initialize_prims()
 
-    # check progress and convergence
-    display_progress(tp, K_TP_1, L_TP_1, p0, pT)
-    converged = check_convergence_TP(pt, tp, K_TP_1, L_TP_1)
+    K_TP_1 = zeros(tp.TPs)   # initialize arrays for new transition path
+    L_TP_1 = zeros(tp.TPs)
+    converged_inner = 0      # convergence flag for inner while loop
+    iter_inner = 1           # iteration counter for inner while loop
+
+    while converged_inner == 0
+        # shoot backwards from T-1 to 1 to solve dynamic household programming
+        @time shoot_backward(pt, tp, r0, rT)
+
+        # shoot forwards from 1 to T to solve cross-sec distribution for age and time
+        @time shoot_forward(pt, tp, r0, K_TP_1, L_TP_1)
+
+        # check progress and convergence
+        converged_inner = check_convergence_TP(iter_inner, pt, tp, K_TP_1, L_TP_1, experiment)
+        iter_inner += 1   # update iteration counter
+    end
+
+    converged_outer, update_TPs = check_convergence_SS(pT, tp, TPs, iter_outer) # check outer convergence, update
+    TPs = update_TPs # update number of transition periods
+    iter_outer += 1
 end
 
 # ------------------------------------------------------------------------ #
 #  (2) compute transition path for expected elimination of social security
 # ------------------------------------------------------------------------ #
-Plots.plot(a_grid, pol_func[:, index_age_20_h] .- a_grid, label = "High productivity")
-Plots.plot!(a_grid, pol_func[:, index_age_20_l] .- a_grid, label = "Low productivity")
