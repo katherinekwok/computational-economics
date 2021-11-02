e8db0f1110b6f0656549a8ff8c4cc9c2d4bbf7d9
Katherine Kwok <kkwok1@swarthmore.edu>
2021-11-02 13:59:43 -0500

    Setting up

---
+++ HW 6/code/main_program.jl
@@ -0,0 +1,34 @@
+# Author: Katherine Kwok
+# Date: November 2, 2021
+
+# This file contains the code for Problem Set 6, where we solve the Hopenhayn-Rogerson
+# model of firm dynamics.
+
+# ------------------------------------------------------------------------ #
+#  (0) load packages and functions
+# ------------------------------------------------------------------------ #
+
+using CSV, DataFrames                                  # load packages for exporting data to csv
+using Parameters, Plots, Printf, LinearAlgebra         # load standard packages
+
+include("model_and_functions.jl")                      # import all functions and strucs
+
+# ------------------------------------------------------------------------ #
+#  (0) initialize algorithm
+# ------------------------------------------------------------------------ #
+
+# ------------------------------------------------------------------------ #
+#  (1) solve for entry market clearing price
+# ------------------------------------------------------------------------ #
+
+# solve value function iteration
+
+# solve entrant's value
+
+# ------------------------------------------------------------------------ #
+#  (2) solve for labor market clearing labor demand and supply
+# ------------------------------------------------------------------------ #
+
+# solve for stationary distribution
+
+# solve for labor demand and supply
---
+++ HW 6/code/model_and_functions.jl
@@ -0,0 +1,33 @@
+# Author: Katherine Kwok
+# Date: November 2, 2021
+
+# This file contains the code for Problem Set 6, where we solve the Hopenhayn-Rogerson
+# model of firm dynamics.
+#
+# The code below is divided into the following sections:
+#
+#   (0) set up strucs and functions to initialize
+#   (1) solve for entry market clearing price
+#   (2) solve for labor market clearing labor demand and supply
+
+# ------------------------------------------------------------------------ #
+#  (0) initialize algorithm
+# ------------------------------------------------------------------------ #
+
+# ------------------------------------------------------------------------ #
+#  (1) solve for entry market clearing price
+# ------------------------------------------------------------------------ #
+
+# solve value function iteration
+
+# solve entrant's value
+
+
+# ------------------------------------------------------------------------ #
+#  (2) solve for labor market clearing labor demand and supply
+# ------------------------------------------------------------------------ #
+
+
+# solve for stationary distribution
+
+# solve for labor demand and supply
