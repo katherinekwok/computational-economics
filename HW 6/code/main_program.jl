35567d6443601becdcf41f5c08deb11a2fee87ec
Katherine Kwok <kkwok1@swarthmore.edu>
2021-11-13 12:08:41 -0600

    Some reorganization

--- HW 6/code/main_program.jl
+++ HW 6/code/main_program.jl
@@ -2,7 +2,16 @@
 # Date: November 2, 2021
 
 # This file contains the code for Problem Set 6, where we solve the Hopenhayn-Rogerson
-# model of firm dynamics.
+# model of firm dynamics. The program implements the following:
+#
+#  (1) solve version 1 of model
+#  (2) solve version 2 of model
+#  (3) compare model moments: price level,
+#                             mass of incumbents/entrants/exits,
+#                             labor aggregate/incumbents/entrants,
+#                             fraction of labor in entrants
+#  (4) plot decision rules of exit 
+
 
 # ------------------------------------------------------------------------ #
 #  (0) load packages and functions
@@ -17,42 +26,7 @@ include("model_and_functions.jl")                      # import all functions an
 #  (0) initialize algorithm
 # ------------------------------------------------------------------------ #
 
-# Primitives: This struct stores the primitives of the model
-@withkw struct Primitives
 
-    β::Float64 = 0.8    # firm discount rate for profits
-    θ::Float64 = 0.64   # persistence value of shock
-    A::Float64 = 1/200  # 
-
-    c_f = 10            # fixed costs for staying in market
-    c_e = 5             # entry costs for entering market
-
-    s::Array{Float64, 1} = [3.98e-4, 3.58, 6.82, 12.18, 18.79] # shock on firm
-    e::Array{Float64, 1} = [1.3e-9, 10, 60, 300, 1000]         # employment levels given shock
-
-    # transition matrix for shock
-    s_trans_mat::Array{Float64, 2} = [0.6598 0.2600 0.0416 0.0331 0.0055;
-                                      0.1997 0.7201 0.0420 0.0326 0.0056;
-                                      0.2000 0.2000 0.5555 0.0344 0.0101;
-                                      0.2000 0.2000 0.2502 0.3397 0.0101;
-                                      0.2000 0.2000 0.2500 0.3400 0.0100]
-
-    # invariant entrant distribution
-    entrant_dist::Array{Float64, 1} = [0.37, 0.4631, 0.1102, 0.0504, 0.0063]
-
-
-end
-
-
-# Results: This struct stores the results of the algorithm
-mutable struct Results
-
-    pol_func::Array{Float64, 1} # exit policy function
-    val_func::Array{Float64, 1} # firm's value function
-
-    stat_dist::
-
-end
 
 
 # ------------------------------------------------------------------------ #
--- HW 6/code/model_and_functions.jl
+++ HW 6/code/model_and_functions.jl
@@ -9,12 +9,48 @@
 #   (0) set up strucs and functions to initialize
 #   (1) solve for entry market clearing price
 #   (2) solve for labor market clearing labor demand and supply
-#   (3) display and plot results 
+#   (3) display and plot results
 
 
 # ------------------------------------------------------------------------ #
 #  (0) initialize algorithm
 # ------------------------------------------------------------------------ #
+
+# Primitives: This struct stores the primitives of the model
+@withkw struct Primitives
+
+    β::Float64 = 0.8    # firm discount rate for profits
+    θ::Float64 = 0.64   # persistence value of shock
+    A::Float64 = 1/200  # employment to population ratio?
+
+    c_f = 10            # fixed costs for staying in market
+    c_e = 5             # entry costs for entering market
+
+    s::Array{Float64, 1} = [3.98e-4, 3.58, 6.82, 12.18, 18.79] # shock on firm
+    e::Array{Float64, 1} = [1.3e-9, 10, 60, 300, 1000]         # firm employment levels given shock
+
+    # transition matrix for firm shock
+    s_trans_mat::Array{Float64, 2} = [0.6598 0.2600 0.0416 0.0331 0.0055;
+                                      0.1997 0.7201 0.0420 0.0326 0.0056;
+                                      0.2000 0.2000 0.5555 0.0344 0.0101;
+                                      0.2000 0.2000 0.2502 0.3397 0.0101;
+                                      0.2000 0.2000 0.2500 0.3400 0.0100]
+
+    # invariant entrant distribution
+    entrant_dist::Array{Float64, 1} = [0.37, 0.4631, 0.1102, 0.0504, 0.0063]
+
+end
+
+
+# Results: This struct stores the results of the algorithm
+mutable struct Results
+
+    pol_func::Array{Float64, 1} # exit policy function
+    val_func::Array{Float64, 1} # firm's value function
+
+    stat_dist::
+
+end
 
 # ------------------------------------------------------------------------ #
 #  (1) solve for entry market clearing price
