d3edce9f50ec12ba304b5337f909a14bbc122149
Katherine Kwok <kkwok1@swarthmore.edu>
2021-11-21 14:48:13 -0600

    Draft of accept reject done

--- HW 9/code/helper_functions.jl
+++ HW 9/code/helper_functions.jl
@@ -20,6 +20,7 @@
 #             integration exercise.
 @with_kw struct Primitives
 
+    # variable definitions
     x_vars = ["score_0", "rate_spread", "i_large_loan", "i_medium_loan",
               "i_refinance", "age_r", "cltv", "dti", "cu", "first_mort_r", "i_FHA",
               "i_open_year2", "i_open_year3", "i_open_year4", "i_open_year5"]
@@ -28,6 +29,7 @@
 
     y_vars = ["i_open_0", "i_open_1", "i_open_2"]
 
+    # initial paramter values
     α0::Float64 = 0
     α1::Float64 = -1
     α2::Float64 = -1
@@ -35,13 +37,17 @@
     γ::Array{Float64, 1} = fill(0.3, size(z_vars, 1))
     ρ::Float64 = 0.5
     σ::Float64 = (1/(1-ρ)^2)
+
+    # simulation related variables
+    n_simu::Int64 = 100 # number of simulations
+
 end
 
 # read_mortgage_data: This function reads and prepares the mortgage data for the
 #                     numerical integration exercise
 
-function read_mortgage_data(file_path::String, params::Primitives)
-    @unpack x_vars, z_vars, y_vars = params
+function read_mortgage_data(file_path::String, param::Primitives)
+    @unpack x_vars, z_vars, y_vars = param
 
     dt = DataFrame(load(file_path)) # load in mortage data set
 
@@ -81,13 +87,12 @@ function transform(grid, upperbound)
     ρ, ρ_prime
 end
 
+# calculate_conditions: This helper function calculates the conditions using
+#                       parameters and observed data the error terms in calculating
+#                       choice probabilities.
+function calculate_conditions(param, x, z)
+    @unpack ρ, σ, α0, α1, α2, γ, β = param
 
-# quadrature: This function applies the quadrature method to find the choice
-#             probability using the Gaussian nodes and weights.
-function quadrature(params, KPU_d1, KPU_d2, x, z)
-    @unpack ρ, σ, α0, α1, α2, γ, β = params
-
-    # calculate a0, a1, a2
     a0_pos = α0 + x' * β + z' * γ
     a1_pos = α1 + x' * β + z' * γ
     a2_pos = α2 + x' * β + z' * γ
@@ -95,6 +100,19 @@ function quadrature(params, KPU_d1, KPU_d2, x, z)
     a0_neg = -α0 - x' * β - z' * γ
     a1_neg = -α1 - x' * β - z' * γ
     a2_neg = -α2 - x' * β - z' * γ
+
+    a0_pos, a1_pos, a2_pos, a0_neg, a1_neg, a2_neg
+
+end
+
+
+# quadrature: This function applies the quadrature method to find the choice
+#             probability using the Gaussian nodes and weights.
+function quadrature(param, KPU_d1, KPU_d2, x, z)
+    @unpack ρ, σ, α0, α1, α2, γ, β = param
+
+    # calculate a0, a1, a2
+    a0_pos, a1_pos, a2_pos, a0_neg, a1_neg, a2_neg = calculate_conditions(param, x, z)
 
     # choice probability of T_i = 1
     prob_1 = cdf.(Normal(), a0_neg/σ)
@@ -123,16 +141,102 @@ end
 
 # quadrature_wrapper: This function calls the quadrature function for each
 #                     observation, and modifies the paramters as given.
-function quadrature_wrapper(X, Z, KPU_d1, KPU_d2, params)
+function quadrature_wrapper(X, Z, KPU_d1, KPU_d2, param)
 
     obs = size(X, 1)           # number of rows/observations
     quad_probs = zeros(obs, 4) # initialize array to store resulting choice probs
 
     # call quadrature function for each observation
     for index in 1:obs
-        quad_probs[index, :] = quadrature(params, KPU_d1, KPU_d2, X[index, :], Z[index, :])
+        quad_probs[index, :] = quadrature(param, KPU_d1, KPU_d2, X[index, :], Z[index, :])
     end
     return quad_probs # return probs
+end
+
+# ---------------------------------------------------------------------------- #
+#   (2) GHK method
+# ---------------------------------------------------------------------------- #
+
+
+
+
+# ---------------------------------------------------------------------------- #
+#   (3) accept/reject method
+# ---------------------------------------------------------------------------- #
+
+# draw_errors: This function samples a specified number of the error terms (ϵ, η)
+#              from a uniform distribution (0,1) then transforms it using the
+#              inverse CDF method to the appropriate distribution. Then, ϵ_i0,
+#              ϵ_i1, ϵ_i2 are computed and returned.
+function draw_errors(param)
+    @unpack n_simu, σ, ρ = param
+
+    # do random draws
+    ϵ_i0 = rand(Uniform(0, 1), n_simu)
+    η_i1 = rand(Uniform(0, 1), n_simu)
+    η_i2 = rand(Uniform(0, 1), n_simu)
+
+    # transform from uniform to normal distributions using inverse CDF (i.e. quantile)
+    ϵ_i0 = quantile.(Normal(0, σ), ϵ_i0)
+    η_i1 = quantile.(Normal(0, 1), η_i1)
+    η_i2 = quantile.(Normal(0, 1), η_i2)
+
+    # compute ϵ_i1 and ϵ_i2
+    ϵ_i1 = ρ*ϵ_i0 + η_i1
+    ϵ_i2 = ρ*ϵ_i1 + η_i2
+
+    return ϵ_i0, ϵ_i1, ϵ_i2
+end
+
+# accept_reject: This function implements the accept reject algorithm for a
+#                single obseration in the data set.
+@everywhere function accept_reject(param, x, z, ϵ_i0, ϵ_i1, ϵ_i2)
+    @unpack n_simu = param
+
+    # initialize the counters for each choice
+    count_T_1 = 0
+    count_T_2 = 0
+    count_T_3 = 0
+    count_T_4 = 0
+
+    # calculate conditions on the error terms
+    a0_pos, a1_pos, a2_pos, a0_neg, a1_neg, a2_neg = calculate_conditions(param, x, z)
+
+    for i in 1:n_simu
+        if ϵ_i0[i] < a0_neg
+            count_T_1 += 1 # if satisfied condition for loan period T = 1
+
+        elseif ϵ_i0[i] < a0_pos && ϵ_i1[i] < a1_neg
+            count_T_2 += 1 # if satisfied condition for loan period T = 2
+
+        elseif ϵ_i0[i] < a0_pos && ϵ_i1[i] < a1_pos && ϵ_i2[i] < a2_neg
+            count_T_3 += 1 # if satisfied condition for loan period T = 3
+
+        elseif ϵ_i0[i] < a0_pos && ϵ_i1[i] < a1_pos && ϵ_i2[i] < a2_pos
+            count_T_4 += 1 # if satisfied condition for loan period T = 4
+        end
+    end
+
+    # calculate choice probability by dividing accepted counts by total # of simulations
+    probs = [count_T_1, count_T_2, count_T_3, count_T_4]./n_simu
+
+    return probs
+end
+
+# accept_reject_wrapper: This function calls the accept_reject function for each
+#                     observation, and modifies the paramters as given.
+function accept_reject_wrapper(X, Z, param)
+
+    obs = size(X, 1)                         # number of rows/observations
+    a_r_probs = SharedArray{Float64}(obs, 4) # initialize array to store resulting choice probs
+
+    ϵ_i0, ϵ_i1, ϵ_i2 = draw_errors(param)    # draw errors
+
+    # call quadrature function for each observation
+    @sync @distributed for index in 1:obs
+        a_r_probs[index, :] = accept_reject(param, X[index, :], Z[index, :], ϵ_i0, ϵ_i1, ϵ_i2)
+    end
+    return a_r_probs # return probs
 end
 
 
@@ -155,10 +259,10 @@ function log_likelihood(X, Z, Y, KPU_d1, KPU_d2, θ; T = 0)
     γ_start = β_end + 1
     γ_end = γ_start + size(default_param.z_vars, 1) -1
 
-    params = Primitives(α0 = θ[1], α1 = θ[2], α2 = θ[3], β = θ[β_start:β_end], γ = θ[γ_start:γ_end], ρ = θ[γ_end+1])
+    param = Primitives(α0 = θ[1], α1 = θ[2], α2 = θ[3], β = θ[β_start:β_end], γ = θ[γ_start:γ_end], ρ = θ[γ_end+1])
 
     # call quadrature_wrapper to get choice probabilities
-    quad_probs = quadrature_wrapper(X, Z, KPU_d1, KPU_d2, params)
+    quad_probs = quadrature_wrapper(X, Z, KPU_d1, KPU_d2, param)
 
     # output sum of log likelihood for given T = 1 or 2 or 3
     for i in 1:size(X)[1]
--- HW 9/code/main_program.jl
+++ HW 9/code/main_program.jl
@@ -13,7 +13,10 @@
 using Parameters, Plots, Printf, LinearAlgebra, Printf # load standard packages
 using StatFiles, DataFrames, CSV                       # load packages for handling data
 using Latexify                                         # load package for outputting results
-using Distributions, Optim, FiniteDiff
+using Distributed, SharedArrays
+
+@everywhere using Distributions, Optim, FiniteDiff, Random
+
 include("helper_functions.jl")                         # load helper functions
 
 root_path = pwd()                                      # set file paths
@@ -23,12 +26,13 @@ mortgage_data_path = data_path * "mortgage_performance_data.dta"
 KPU_d1_path = data_path * "KPU_d1_l20.csv"
 KPU_d2_path = data_path * "KPU_d2_l20.csv"
 
+Random.seed!(12032020) # set seed
 
 # ---------------------------------------------------------------------------- #
 #   (0) load data and set up variables
 # ---------------------------------------------------------------------------- #
-params = Primitives()
-X, Z, Y = read_mortgage_data(mortgage_data_path, params)
+param = Primitives()
+X, Z, Y = read_mortgage_data(mortgage_data_path, param)
 KPU_d1 = read_KPU_data(KPU_d1_path)
 KPU_d2 = read_KPU_data(KPU_d2_path)
 
@@ -37,7 +41,7 @@ KPU_d2 = read_KPU_data(KPU_d2_path)
 #   (1) use quadrature method to predict choice probabilities
 # ---------------------------------------------------------------------------- #
 
-quad_probs = quadrature_wrapper(X, Z, KPU_d1, KPU_d2, params)
+quad_probs = quadrature_wrapper(X, Z, KPU_d1, KPU_d2, param)
 
 # ---------------------------------------------------------------------------- #
 #   (2) use GHK method to predict choice probabilities
@@ -49,11 +53,13 @@ quad_probs = quadrature_wrapper(X, Z, KPU_d1, KPU_d2, params)
 #   (3) use accept/reject method to predict choice probabilities
 # ---------------------------------------------------------------------------- #
 
+a_r_probs = accept_reject_wrapper(X, Z, param)
+
 
 # ---------------------------------------------------------------------------- #
 #   (4) run maximum likelihood using quadrature method
 # ---------------------------------------------------------------------------- #
 
-θ_init = vcat([params.α0, params.α1, params.α2], params.β, params.γ, [params.ρ])
+θ_init = vcat([param.α0, param.α1, param.α2], param.β, param.γ, [param.ρ])
 θ_bfgs_Y_0 = @time optimize(θ -> -log_likelihood(X, Z, Y, KPU_d1, KPU_d2, θ; T = 1), θ_init, BFGS(); inplace = false)
 Optim.minimizer(θ_bfgs_Y_0)
