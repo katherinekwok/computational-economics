b39a3d7ea214c81c6f689a0fd83326b231fdecb4
Katherine Kwok <kkwok1@swarthmore.edu>
2021-11-28 11:55:55 -0600

    Pushing reading notes and outline of paper in write up

---
+++ final project/code/main_program.jl
@@ -0,0 +1,5 @@
+# Author: Katherine Kwok
+# Date: November 27, 2021
+
+# This program replicates the code in Arellano (2008). The main steps in the
+# algorithm are:
---
+++ final project/code/model_and_functions.jl
@@ -0,0 +1,5 @@
+# Author: Katherine Kwok
+# Date: November 27, 2021
+
+# This file contains the declaration of structs and helper functionst that are
+# used to replicate the code in Arellano (2008).
