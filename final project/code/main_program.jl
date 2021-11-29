b96f35f082136282e682cff7dab34912f6225ec9
Katherine Kwok <kkwok1@swarthmore.edu>
2021-11-28 20:20:45 -0600

    Started initializing and some pseudocode/comments

--- final project/code/main_program.jl
+++ final project/code/main_program.jl
@@ -1,5 +1,24 @@
-# Author: Katherine Kwok
-# Date: November 27, 2021
+"""
+Author: Katherine Kwok
+Date: November 27, 2021
 
-# This program replicates the code in Arellano (2008). The main steps in the
-# algorithm are:
+This program replicates the code in Arellano (2008). The main steps in the
+algorithm are
+
+1. Initialize the parameter values for β, θ, y_hat and an asset grid
+   with 200 grid points.
+
+2. Initialize q_0 = 1/(1+r) for all asset levels B' and income levels y.
+
+3. Use value function iteration to solve for the value function and policy
+   functions for c, B'. Using the results, construct the optimal income sets
+   for repayment A and default D.
+
+4. Update the bond price q_1 so that lenders break-even. Compare q_1 with q_0:
+   if the bond price vectors are within a tolerance value, then we have converged.
+   If not, update bond prices and repeat value function iteration.
+   
+5. Use the data generated above to compute business cycle statistics. If they
+   match with the Argentina data, stop, if not, repeat the procedure with adjusted
+   parameter values and grid points.
+"""
--- final project/code/model_and_functions.jl
+++ final project/code/model_and_functions.jl
@@ -3,3 +3,55 @@
 
 # This file contains the declaration of structs and helper functionst that are
 # used to replicate the code in Arellano (2008).
+
+
+# ---------------------------------------------------------------------------- #
+# (0) Initialize primitives and results structs
+# ---------------------------------------------------------------------------- #
+
+@with_kw struct Primitives
+
+    r::Float64 = 0.017 # risk free interest rate
+    σ::Float64 = 2     # risk aversion
+    ρ::Float64 = 0.945 # coefficient for log-normal AR(1) process for income
+    η::Float64 = 0.025 # variance for error term in log-normal AR(1) process for income
+
+    β::Float64 = 0.953 # discount factor
+    θ::Float64 = 0.282 # re-entry probability after defaulting
+
+    y_hat::Float64 = 0.969 # NOTE: Not sure about this one
+
+    # note should define B grid and y grid
+
+end
+
+
+mutable struct Results
+
+    q::Array{Float64, 2}  # bond price schedule
+
+    pol_func_c::Array{Float64, 2} # consumption
+    pol_func_B::Array{Float64, 2} # assets
+
+    val_func::Array{Float64, 2}   # value function
+
+    A::Array{Float64}             # income set where paying back is optimal
+    D::Array{Float64}             # income set where defaulting is optimal
+
+end
+
+# ---------------------------------------------------------------------------- #
+# (1) Value function iteration
+# ---------------------------------------------------------------------------- #
+
+
+
+# ---------------------------------------------------------------------------- #
+# (2) Bond price converence
+# ---------------------------------------------------------------------------- #
+
+
+
+# ---------------------------------------------------------------------------- #
+# (3) Business cycle statistics
+# ---------------------------------------------------------------------------- #
