edecfc3beb0121e1f4c63853f5b09b10597180ab
Katherine Kwok <kkwok1@swarthmore.edu>
2021-11-29 11:55:52 -0600

    Pushing small update

--- final project/code/main_program.jl
+++ final project/code/main_program.jl
@@ -17,8 +17,18 @@ algorithm are
 4. Update the bond price q_1 so that lenders break-even. Compare q_1 with q_0:
    if the bond price vectors are within a tolerance value, then we have converged.
    If not, update bond prices and repeat value function iteration.
-   
+
 5. Use the data generated above to compute business cycle statistics. If they
    match with the Argentina data, stop, if not, repeat the procedure with adjusted
    parameter values and grid points.
 """
+
+
+# ---------------------------------------------------------------------------- #
+# (1) Benchmark model
+# ---------------------------------------------------------------------------- #
+
+
+# ---------------------------------------------------------------------------- #
+# (2) General model
+# ---------------------------------------------------------------------------- #
