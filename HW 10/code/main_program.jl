e85589a324d2dd9e1adfab2390297c7a69df0908
Katherine Kwok <kkwok1@swarthmore.edu>
2021-12-01 11:18:18 -0600

    Setting up code files and comments

---
+++ HW 10/code/main_program.jl
@@ -0,0 +1,5 @@
+# Author: Katherine Kwok
+# Date: December 5, 2021
+
+# This file contains the code for Problem Set 3 (for JF's portion) a.k.a.
+# Problem Set 10 overall. The main program implements the BLP algorithm.
